3f69632f64c4312855ab92ae5d55b5c1aafe41b3
Aura Frizzati <auraf85@yahoo.it>
2025-02-24 20:10:20 +0000

    updated

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,8 +1,40 @@
 with 
 
-source as {
+source as (
 
     select * from {{ source('staging', 'green_trip_data')}}
-}
+)
+,renamed as (
 
-select * from source
+    select 
+        vendor_id AS vendorid,
+        pickup_datetime AS lpep_pickup_datetime,
+        dropoff_datetime AS lpep_dropoff_datetime,
+        store_and_fwd_flag,
+        rate_code AS ratecode_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        airport_fee,
+        total_amount,
+        payment_type,
+        coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type2,
+        {{ get_payment_type_description("payment_type") }} as payment_type_description,
+        distance_between_service,
+        time_between_service,
+        trip_type,
+        imp_surcharge,
+        pickup_location_id AS pulocationid,
+        dropoff_location_id AS dolocationsid,
+        data_file_year,
+        data_file_month
+
+    from source
+)
+
+select * from renamed
